6111473725ce350a9ceb7be97415d4414748ae2a
Letícia Polari <79110802+Polaris851@users.noreply.github.com>
2022-09-17 23:46:39 -0300

    Delete Front-End/JavaScript/Manipulacao Dom directory

--- Front-End/JavaScript/Manipulacao Dom/functionAnonima/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('calcular');
-const text = document.querySelector('.resultado');
-
-button.addEventListener('click', () => {
-    text.innerHTML = 'Fui clicado';
-})
